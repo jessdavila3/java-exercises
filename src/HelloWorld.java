6a4168af1e90cb8a26e8e2645dd71eb75ae4c445
Jesse Davila <jessedavila3@gmail.com>
2016-11-11 16:54:38 -0600

    hello world exercise, using scanner object

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,48 @@
 /**
  * Created by jessedavila on 11/11/16.
  */
+
+import java.util.Scanner;
+
 public class HelloWorld {
-    // psvm
+
     public static void main(String[] args) {
         //sout
+        boolean keepGoing = true;
+        String userInputOne, userInputTwo, userInputThree;
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Hello world!");
+        System.out.print("enter 3 words: ");
+        userInputOne = sc.next();
+        userInputTwo = sc.next();
+        userInputThree = sc.next();
+
+        System.out.println("You entered: ");
+        System.out.println(userInputOne);
+        System.out.println(userInputTwo);
+        System.out.println(userInputThree);
+        while (keepGoing) {
+            int userInput;
+            double length, width;
+            System.out.println("\nwhat are your dimensions?");
+            length = sc.nextDouble();
+            width = sc.nextDouble();
+            System.out.println("The area is: " + rectangle(length,width) + ".");
+            System.out.println("keep going?\nPress 1 for yes\nPress 0 for no");
+            userInput = sc.nextInt();
+            if (userInput != 1) {
+                keepGoing = false;
+                System.out.println("thanks for playing");
+            } else {
+                keepGoing = true;
+                System.out.println("hey");
+            }
+        }
 
     }
+
+    private static double rectangle(double len, double width) {
+        return len * width;
+    }
+
 }
