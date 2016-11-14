5a56a18f12371adb2eb2e8cb68221517a390fc68
Jesse Davila <jessedavila3@gmail.com>
2016-11-14 10:32:48 -0600

    FizzBuzz basic loop

--- src/FizzBuzzer.java
+++ src/FizzBuzzer.java
@@ -3,6 +3,16 @@
  */
 public class FizzBuzzer {
     public static void main(String[] args) {
-        System.out.println("fizzbuzz!");
+        for (int num = 1; num <= 100; num++) {
+            if (num % 3 == 0 && num % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (num % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (num % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(num);
+            }
+        }
     }
 }
