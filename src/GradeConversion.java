ddd8fdfac011223232e4bea75d68e6ddc910441e
Jesse Davila <jessedavila3@gmail.com>
2016-11-14 11:02:04 -0600

    working repeat function, though it might be able to be written more simply

--- src/GradeConversion.java
+++ src/GradeConversion.java
@@ -8,22 +8,40 @@ public class GradeConversion {
         Scanner sc = new Scanner(System.in);
         double userGrade;
         char letterGrade;
+        int again;
+        boolean keepGoing = false;
 
-        System.out.println("What was your grade?");
-        userGrade = sc.nextDouble();
 
-        if (userGrade >= 88) {
-            letterGrade = 'A';
-        } else if (userGrade >= 80) {
-            letterGrade = 'B';
-        } else if (userGrade >= 67) {
-            letterGrade = 'C';
-        } else if (userGrade >= 60) {
-            letterGrade = 'D';
-        } else {
-            letterGrade = 'F';
-        }
+
+        do {
+            System.out.println("What was your grade?");
+            userGrade = sc.nextDouble();
+
+            if (userGrade >= 88) {
+                letterGrade = 'A';
+            } else if (userGrade >= 80) {
+                letterGrade = 'B';
+            } else if (userGrade >= 67) {
+                letterGrade = 'C';
+            } else if (userGrade >= 60) {
+                letterGrade = 'D';
+            } else {
+                letterGrade = 'F';
+            }
 
         System.out.println("Your letter grade is: " + letterGrade);
+
+        System.out.println("keep going?\nType '1' to continue.");
+        again = sc.nextInt();
+
+        if (again == 1) {
+            keepGoing = true;
+        } else if (again != 1) {
+            keepGoing = false;
+        }
+
+        } while (keepGoing);
+
+        System.out.println("Thanks for playing...");
     }
 }
