890d2c3e5545ea415bf1405914a8669ed8821c98
Jesse Davila <jessedavila3@gmail.com>
2016-11-15 15:07:54 -0600

    exceptions for dice rolling

--- src/DiceRolling.java
+++ src/DiceRolling.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -5,21 +6,40 @@ import java.util.Scanner;
  */
 public class DiceRolling {
     public static void main(String[] args) {
-        int diceSides;
-        int dieOne = 1, dieTwo = 1;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("how many sides are on the dice?");
-        diceSides = sc.nextInt();
-        dieOne = rollDie(dieOne,diceSides);
-        dieTwo = rollDie(dieTwo,diceSides);
-
-        System.out.println("1: " + dieOne);
-        System.out.println("2: " + dieTwo);
+        diceGame();
 
     }
 
     public static int rollDie(int die, int diceSides) {
         die = (int)Math.ceil(Math.random() * diceSides);
         return die;
+    }
+
+    public static void diceGame() {
+        int diceSides;
+        String keepGoing = "y";
+        Scanner sc = new Scanner(System.in);
+        while (keepGoing.equals("y")) {
+            int dieOne = 1, dieTwo = 1;
+            System.out.println("how many sides are on the dice?");
+            while (keepGoing.equals("y")) {
+                try {
+                    diceSides = sc.nextInt();
+                    dieOne = rollDie(dieOne,diceSides);
+                    dieTwo = rollDie(dieTwo,diceSides);
+                    System.out.println("Die 1: " + dieOne);
+                    System.out.println("Die 2: " + dieTwo);
+                    System.out.println("Total: " + (dieOne + dieTwo));
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("nope");
+                }
+            }
+            System.out.println("keep going? (y/n)");
+            keepGoing = sc.next();
+
+        }
+        System.out.println("thanks for playing");
+
     }
 }
---
+++ src/ExceptionsErrorHandling.java
@@ -0,0 +1,16 @@
+/**
+ * Created by jessedavila on 11/15/16.
+ */
+import java.util.Scanner;
+
+public class ExceptionsErrorHandling {
+    public static void getInfo() {
+        String name =
+        int age;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("what's the student's name?");
+        name = sc.next();
+        System.out.println("how old are they?");
+        age = sc.nextInt();
+    }
+}
