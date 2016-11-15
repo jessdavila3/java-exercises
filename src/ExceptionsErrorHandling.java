0199f34d2bbab221d9f78b0c038dabdff4f75283
Jesse Davila <jessedavila3@gmail.com>
2016-11-15 15:32:22 -0600

    Student info

--- src/ExceptionsErrorHandling.java
+++ src/ExceptionsErrorHandling.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 
 public class ExceptionsErrorHandling {
     public static void getInfo() {
-        String name =
+        String name, whyCode;
         int age;
         Scanner sc = new Scanner(System.in);
         System.out.println("what's the student's name?");
         name = sc.next();
         System.out.println("how old are they?");
         age = sc.nextInt();
+        System.out.println("Why program?");
+        whyCode = sc.nextLine();
+        studentInfo(name,age,whyCode);
+    }
+
+    public static void studentInfo(String name, int age, String why) {
+        System.out.println("Your name is "+name+". You're "+age+" years old.\nYou want to program because\n"+why);
+    }
+
+    public static void main(String[] args) {
+        getInfo();
     }
 }
