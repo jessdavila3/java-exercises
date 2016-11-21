7bd52d59cda219e402bd244084371d4a6068667b
Jesse Davila <jessedavila3@gmail.com>
2016-11-21 16:48:28 -0600

    Batting Stats array arrays to hold atBat and numOfBases, don't know if should make a 2d array

--- src/BattingStats.java
+++ src/BattingStats.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Created by jessedavila on 11/21/16.
  */
 public class BattingStats {
+
+    public static void main(String[] args) {
+        int numAtBats, numOfBases;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many are at bat?");
+        numAtBats = input.nextInt();
+        int[] average = new int[numAtBats];
+
+        for (int numBats : average) {
+            System.out.println("bases?");
+            numBats = input.nextInt();
+        }
+
+        System.out.println(average);
+
+    }
+
 }
