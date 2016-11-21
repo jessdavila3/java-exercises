87f03ddf95a2ef911129e11e97541da0b1cb0e28
Jesse Davila <jessedavila3@gmail.com>
2016-11-21 14:20:50 -0600

    added a random element to picking a color discount.

--- src/LuckyColor.java
+++ src/LuckyColor.java
@@ -5,17 +5,34 @@ import java.util.Scanner;
  * Created by jessedavila on 11/21/16.
  */
 public class LuckyColor {
-
     public static void main(String[] args) {
-        double totalCost;
+        double totalCost,discount;
 
         DecimalFormat df = new DecimalFormat("###0.00");
         Scanner input = new Scanner(System.in);
 
+        int random = (int) Math.ceil(Math.random() * 6);
+
         System.out.println("How much?");
         totalCost = input.nextDouble();
-        System.out.println("Today you get a green discount...");
-        totalCost = totalCost - Colors.GREEN.applyDiscount(totalCost);
+        if (random == 1) {
+            discount = Colors.RED.applyDiscount(totalCost);
+        } else if (random == 2) {
+            discount = Colors.BLUE.applyDiscount(totalCost);
+        } else if (random == 3) {
+            discount = Colors.YELLOW.applyDiscount(totalCost);
+        } else if (random == 4) {
+            discount = Colors.GREEN.applyDiscount(totalCost);
+        } else if (random == 5) {
+            discount = Colors.PURPLE.applyDiscount(totalCost);
+        } else if (random == 6) {
+            discount = Colors.PINK.applyDiscount(totalCost);
+        } else {
+            discount = 0;
+        }
+        totalCost -= discount;
         System.out.println("total: $" + df.format(totalCost));
+
+        input.close();
     }
 }
