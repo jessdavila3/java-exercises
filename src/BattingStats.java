e73ba4281724decad933a5e579b6fb7372970025
Jesse Davila <jessedavila3@gmail.com>
2016-11-22 09:52:21 -0600

    total variable and bases method added

--- src/BattingStats.java
+++ src/BattingStats.java
@@ -6,20 +6,31 @@ import java.util.Scanner;
 public class BattingStats {
 
     public static void main(String[] args) {
-        int numAtBats, numOfBases;
+        int numAtBats;
+        int total = 0;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("How many are at bat?");
+        System.out.println("How many at bats?");
         numAtBats = input.nextInt();
-        int[] average = new int[numAtBats];
+        int[][] average = new int[numAtBats][numAtBats];
 
-        for (int numBats : average) {
-            System.out.println("bases?");
-            numBats = input.nextInt();
+        for (int i = 0; i < numAtBats; i++) {
+            average[i][i] = numBases();
+            System.out.println(average[i][i]);
+            total += average[i][i];
         }
+        total /= numAtBats;
 
-        System.out.println(average);
+        System.out.println("Your average is " + total);
+    }
+
+    public static int numBases() {
+        Scanner input = new Scanner(System.in);
+        int numOfBases;
+        System.out.println("How many bases?");
+        numOfBases = input.nextInt();
+        return numOfBases;
 
     }
 
