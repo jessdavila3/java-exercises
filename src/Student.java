5e8894227a675c0dd23e34f5d6f1844c32de7ee8
Jesse Davila <jessedavila3@gmail.com>
2016-11-28 14:20:00 -0600

    Student Record App

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jessedavila on 11/28/16.
+ */
+public class Student {
+    String firstName;
+    String lastName;
+    int score;
+
+    public Student(String firstName, String lastName, int score) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.score = score;
+    }
+}
+
---
+++ src/StudentScores.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jessedavila on 11/28/16.
+ */
+public class StudentScores {
+    public static void main(String[] args) {
+
+        Student newStudent = new Student();
+
+        // use the Scanner;
+        newStudent.firstName = "Bob";
+        newStudent.lastName = "Belcher";
+        newStudent.score = 5;
+    }
+}
