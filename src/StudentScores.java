31c81095982618165566bec4b97264553c8a9d3f
Jesse Davila <jessedavila3@gmail.com>
2016-11-29 13:04:30 -0600

    getScore function created and is recursive to make sure you enter a correct integer for score

--- src/StudentScores.java
+++ src/StudentScores.java
@@ -5,7 +5,8 @@ import java.util.*;
  */
 public class StudentScores {
     public static void main(String[] args) {
-        String firstName, lastName, keepGoing;
+        String firstName, lastName;
+        String keepGoing = "y";
         int score;
 
         Scanner sc = new Scanner(System.in);
@@ -18,14 +19,19 @@ public class StudentScores {
             firstName = sc.nextLine().trim();
             System.out.println("And the last name?");
             lastName = sc.nextLine().trim();
-            System.out.println("Score?");
-            score = Integer.parseInt(sc.nextLine());
+            if (firstName.trim().equalsIgnoreCase("")||lastName.trim().equalsIgnoreCase("")) {
+                System.out.println("Please enter a valid first and last name.");
+            } else {
+                score = getScore(sc);
 
-            Student newStudent = new Student(firstName,lastName,score);
-            students.add(newStudent);
 
-            System.out.println("Continue?");
-            keepGoing = sc.nextLine();
+                Student newStudent = new Student(firstName,lastName,score);
+                students.add(newStudent);
+
+                System.out.println("Continue?");
+                keepGoing = sc.nextLine();
+            }
+
         } while (keepGoing.equalsIgnoreCase("y"));
 
         Collections.sort(students); //sorts the last names then first names if need be.
@@ -36,8 +42,17 @@ public class StudentScores {
             System.out.println("Score: " + student.score);
             System.out.println("-----");
         }
-
-
-
     }
+    public static int getScore(Scanner sc) {
+        int score = 0;
+        try {
+            System.out.println("Enter a score between 0 and 100");
+            score = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e){
+            System.out.println("enter an integer");
+            getScore(sc);
+        }
+        return score;
+    }
+
 }
